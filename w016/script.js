38bbab7cf5ccf9f12815e739ef04a86292f1de0b
amamagi <amausagiii@gmail.com>
2021-03-12 00:16:22 +0900

    refactor

--- w016/script.js
+++ w016/script.js
@@ -4,6 +4,68 @@ onload = function(){
     c.height = 300;
     var gl = c.getContext('webgl');
 
+    gl.clearColor(.0,.0,.0,1.);
+    gl.clearDepth(1.);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var v_shader = create_shader('vs');
+    var f_shader = create_shader('fs');
+    var prg = create_program(v_shader, f_shader);
+
+    var attLocation = new Array(2);
+    attLocation[0] = gl.getAttribLocation(prg, 'position');
+    attLocation[1] = gl.getAttribLocation(prg, 'color');
+    
+    var attStride = new Array(2);
+    attStride[0] = 3;
+    attStride[1] = 4;
+
+    var vertex_position = [
+        0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+        -1.0, 0.0, 0.0
+    ];
+
+    var vertex_color = [
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0
+    ];
+
+    var position_vbo = create_vbo(vertex_position);
+    var color_vbo = create_vbo(vertex_color);
+    set_attribute([position_vbo, color_vbo], attLocation, attStride);
+
+    var m = new matIV();
+    var Matrix = m.identity(m.create());
+    
+    var mMatrix = m.identity(m.create());
+    var vMatrix = m.identity(m.create());
+    var pMatrix = m.identity(m.create());
+    var vpMatrix = m.identity(m.create());
+    var mvpMatrix = m.identity(m.create());
+
+    m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
+    m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
+    m.multiply(pMatrix, vMatrix, vpMatrix);
+    
+    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
+    
+    // 1つ目のモデル
+    m.translate(mMatrix, [1.5, 0.0, 0.0], mMatrix);
+    m.multiply(vpMatrix, mMatrix, mvpMatrix);
+    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    // 2つ目のモデル
+    m.identity(mMatrix);
+    m.translate(mMatrix, [-1.5, 0.0, 0.0], mMatrix);
+    m.multiply(vpMatrix, mMatrix, mvpMatrix);
+    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    gl.flush();
+    
     function create_shader(id){
         var shader;
         var scriptElement = document.getElementById(id);
@@ -50,73 +112,11 @@ onload = function(){
         return vbo;
     }
     
-
-    gl.clearColor(.0,.0,.0,1.);
-    gl.clearDepth(1.);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    var v_shader = create_shader('vs');
-    var f_shader = create_shader('fs');
-    var prg = create_program(v_shader, f_shader);
-
-    var attLocation = new Array(2);
-    attLocation[0] = gl.getAttribLocation(prg, 'position');
-    attLocation[1] = gl.getAttribLocation(prg, 'color');
-    
-    var attStride = new Array(2);
-    attStride[0] = 3;
-    attStride[1] = 4;
-
-    var vertex_position = [
-        0.0, 1.0, 0.0,
-        1.0, 0.0, 0.0,
-        -1.0, 0.0, 0.0
-    ];
-
-    var vertex_color = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0
-    ];
-
-    var position_vbo = create_vbo(vertex_position);
-    var color_vbo = create_vbo(vertex_color);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, position_vbo);
-    gl.enableVertexAttribArray(attLocation[0]);
-    gl.vertexAttribPointer(attLocation[0], attStride[0], gl.FLOAT, false, 0, 0);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, color_vbo);
-    gl.enableVertexAttribArray(attLocation[1]);
-    gl.vertexAttribPointer(attLocation[1], attStride[1], gl.FLOAT, false, 0, 0);
-
-    var m = new matIV();
-    var Matrix = m.identity(m.create());
-    
-    var mMatrix = m.identity(m.create());
-    var vMatrix = m.identity(m.create());
-    var pMatrix = m.identity(m.create());
-    var vpMatrix = m.identity(m.create());
-    var mvpMatrix = m.identity(m.create());
-
-    m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
-    m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
-    m.multiply(pMatrix, vMatrix, vpMatrix);
-    
-    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
-    
-    // 1つ目のモデル
-    m.translate(mMatrix, [1.5, 0.0, 0.0], mMatrix);
-    m.multiply(vpMatrix, mMatrix, mvpMatrix);
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-
-    // 2つ目のモデル
-    m.identity(mMatrix);
-    m.translate(mMatrix, [-1.5, 0.0, 0.0], mMatrix);
-    m.multiply(vpMatrix, mMatrix, mvpMatrix);
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-
-    gl.flush();
+    function set_attribute(vbo, attL, attS){
+        for(var i in vbo){
+            gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
+            gl.enableVertexAttribArray(attL[i]);
+            gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+        }
+    }
 }
